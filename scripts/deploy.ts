a9cb7e8605e792848198efc796cbcbee7b661984
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2024-12-02 22:58:43 +0300

    added the greetings contract

---
+++ scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Greeting = await ethers.getContractFactory("Greeting");
+  
+  const contract = await Greeting.deploy();
+  
+  await contract.deploymentTransaction()?.wait();
+  
+  console.log(`Contract deployed to: ${await contract.getAddress()}`);
+}
+
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
